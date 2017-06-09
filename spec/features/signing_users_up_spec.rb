97fa9a05bfb6231807e0224e4b4788b07f6cb512
pklonowski <klonowskip@hotmail.com>
2017-06-09 20:46:43 +0000

    Implement user signin feature

--- spec/features/signing_users_up_spec.rb
+++ spec/features/signing_users_up_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 RSpec.feature "Users Sign Up" do
   scenario do
     visit "/"
-    click_link "Sign Up"
+    click_link "Sign up"
     fill_in "Email", with: "john@example.com"
     fill_in "Password", with: "password"
     fill_in "Password confirmation", with: "password"
