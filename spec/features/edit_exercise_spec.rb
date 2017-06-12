f8381a886e133132270b7e52df3abc360729dce0
pklonowski <klonowskip@hotmail.com>
2017-06-12 19:27:38 +0000

    Implement editing exercise

--- spec/features/edit_exercise_spec.rb
+++ spec/features/edit_exercise_spec.rb
@@ -16,12 +16,12 @@ RSpec.feature "Editing Exercise" do
     
     path = "/users/#{@owner.id}/exercises/#{@owner_ex.id}/edit"
     link = "a[href=\'#{path}\']"
-    find(link).click_link
+    find(link).click
     
     fill_in "Duration", with: 45
     click_button "Update Exercise"
     
-    expect(page).to have_link("Exercise has been updated")
+    expect(page).to have_content("Exercise has been updated")
     expect(page).to have_content(45)
     expect(page).not_to have_content(48)
   end
