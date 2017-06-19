4ee28e0dd09d9bd36c00c8ea7c818520ca776b7e
pklonowski <klonowskip@hotmail.com>
2017-06-19 05:33:16 +0000

    Implement unfollowing friends

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -14,6 +14,16 @@ class FriendshipsController < ApplicationController
     redirect_to root_path
   end
   
+  def destroy
+    @friendship = Friendship.find(params[:id])
+    if @friendship.destroy
+      flash[:notice] = "#{@friendship.friend.full_name} unfollowed."
+    else
+      flash.now[:alert] = "#{@friendship.friend.full_name} could not be unfollowed."
+    end
+    redirect_to user_exercises_path(current_user)
+  end
+  
 private
 
   def friendship_params
